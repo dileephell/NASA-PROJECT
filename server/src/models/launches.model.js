2d15cb2e601afcbd4d01bb3819bfddc7ebf08b1a
Dileep Singh <dileep.singh@merrillcorp.com>
2021-07-20 16:18:02 +0530

    POST /launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber : 100,
     mission: 'Kepler Exploration X',
@@ -16,6 +18,21 @@ launches.set(launch.flightNumber, launch);
 function getAllLaunches(){
     return Array.from(launches.values());
 }
+
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber,
+        Object.assign(launch, {
+            success: true,
+            upcoming: true,
+            customers: ['Zero to Mastery', 'NASA'],
+            flightNumber: launch.flightNumber,
+        })
+        );
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 };
